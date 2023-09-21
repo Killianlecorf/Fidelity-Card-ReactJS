1789bd5e2eecee4ced633b724a04fc690b7aabfb
killian-lecorf <killianlecorf@outlook.fr>
2023-09-21 09:05:43 +0200

    responsive & end accueilEntreprisePanel

--- src/Components/AccueilEntreprisePanel/AccueilEntreprisePanel.tsx
+++ src/Components/AccueilEntreprisePanel/AccueilEntreprisePanel.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import BackPage from '../UI-Kit/BackPage';
 import FieldCard from '../UI-Kit/FieldCard';
 import { HiOutlineUserGroup } from "react-icons/hi2";
+import { BsShop } from "react-icons/bs";
+import { FiSettings } from "react-icons/fi";
 
 const AccueilEntreprisePanel = () => {
+
     return (
         <div className='AccueilEntreprisePanel'>
             <div className="titleEntreprisePanel">
@@ -12,7 +15,10 @@ const AccueilEntreprisePanel = () => {
             <BackPage urlRedirection='/entreprise' />
             <div className="ChoisePanelContent">
                 <FieldCard icon={<HiOutlineUserGroup/>} title='Base de donnée client' />
+                <FieldCard icon={<BsShop/>} title='Vos Boutiques' />
+                <FieldCard icon={<FiSettings/>} title="Option de l'entreprise" />
             </div>
+            
         </div>
     );
 };
--- src/Pages/AccueilEntreprise/AccueilEntreprise.tsx
+++ src/Pages/AccueilEntreprise/AccueilEntreprise.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../Components/Header';
 import NavBar from '../../Components/NavBar';
 import AccueilEntreprisePanel from '../../Components/AccueilEntreprisePanel';
+import ModalMobile from '../../Components/ModalMobile';
 
 const AccueilEntreprise = () => {
+
+    const [isOpenModal, setIsOpenModal] = useState<boolean>(false)
+
+    const openModal = () => {
+        if (isOpenModal === true) {
+        return <ModalMobile isOpenModal={isOpenModal} setIsOpenModal={setIsOpenModal}/>
+        }
+        return
+    }
+
     return (
         <div>
-            <Header/>
+            <Header isOpenModal={isOpenModal} setIsOpenModal={setIsOpenModal} />
             <div className="displayDashboard">
                 <NavBar />
                 <AccueilEntreprisePanel />
             </div>
+            {openModal()}
         </div>
     );
 };
